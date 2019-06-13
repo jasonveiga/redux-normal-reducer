3606b937a96dd50f48513fbd3bc4e30a9d695967
Jason Veiga <jasonveiga@me.com>
2019-06-12 22:25:37 -0600

    added configs for "safe" operations

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 const DEFAULT_ACTION_TYPES = {
+  clear: false,
   create: false,
+  createAll: false,
   add: false,
+  addAll: false,
   replace: false,
+  replaceAll: false,
   move: false,
   addOrReplace: false,
+  addOrReplaceAll: false,
   merge: false,
-  remove: false
+  mergeAll: false,
+  remove: false,
+  removeAll: false
 };
 
-const DEFAULT_OPTIONS = { creator: defaultCreator, merger: shallowMerge };
+const DEFAULT_OPTIONS = {
+  creator: defaultCreator,
+  merger: shallowMerge,
+  safe: {
+    add: true,
+    addAll: true,
+    replace: true,
+    replaceAll: true,
+    move: true,
+    merge: true,
+    mergeAll: true
+  }
+};
 
 function defaultCreator(data = {}) {
   return { ...data };
@@ -24,15 +43,157 @@ const add = (state, data) => ({
   byId: { ...state.byId, [data.id]: data }
 });
 
+function addSafe(state, data) {
+  if (state.byId.hasOwnProperty(data.id)) {
+    throw new Error(`can't add id ${data.id}: already exists`);
+  }
+
+  return add(state, data);
+}
+
+const addAll = (state, data) => ({
+  ...state,
+  allIds: [...state.allIds, ...data.map(x => x.id)],
+  byId: data.reduce(
+    (byId, x) => {
+      byId[x.id] = x;
+      return byId;
+    },
+    { ...state.byId }
+  )
+});
+
+function addAllSafe(state, data) {
+  let existing = data
+    .filter(x => state.byId.hasOwnProperty(x.id))
+    .map(x => x.id);
+
+  if (existing.length) {
+    throw new Error(`can't add ids ${existing.join(", ")}: they already exist`);
+  }
+
+  return addAll(state, data);
+}
+
 const create = (state, data, creator) => add(state, creator(data));
+
+function createSafe(state, data, creator) {
+  if (state.byId.hasOwnProperty(data.id)) {
+    throw new Error(`can't create id ${data.id}: already exists`);
+  }
+
+  return create(state, data, creator);
+}
+
+function createAll(state, data, creator) {
+  data = data.map(creator);
+  return {
+    allIds: [...state.allIds, data.map(x => x.id)],
+    byId: data.reduce((byId, x) => {
+      byId[x.id] = x;
+      return byId;
+    }, state.byId)
+  };
+}
+
+function createAllSafe(state, data, creator) {
+  let existing = data
+    .filter(x => state.byId.hasOwnProperty(x.id))
+    .map(x => x.id);
+
+  if (existing.length) {
+    throw new Error(
+      `can't create ids ${existing.join(", ")}: they already exist`
+    );
+  }
+
+  return createAll(state, data, creator);
+}
 
 const replace = (state, data) => ({
   ...state,
   byId: { ...state.byId, [data.id]: data }
 });
 
+function replaceSafe(state, data) {
+  if (!state.byId.hasOwnProperty(data.id)) {
+    throw new Error(`can't replace id ${data.id}: not found`);
+  }
+
+  return replace(state, data);
+}
+
+const replaceAll = (state, data) => ({
+  ...state,
+  byId: data.reduce(
+    (byId, x) => {
+      byId[x.id] = x;
+      return byId;
+    },
+    { ...state.byId }
+  )
+});
+
+function replaceAllSafe(state, data) {
+  let notExisting = data
+    .filter(x => !state.byId.hasOwnProperty(x.id))
+    .map(x => x.id);
+
+  if (notExisting.length) {
+    throw new Error(
+      `can't replace ids ${notExisting.join(", ")}: they're not found`
+    );
+  }
+
+  return replaceAll(state, data);
+}
+
+const addOrReplace = (state, data) =>
+  state.byId.hasOwnProperty(data.id) ? replace(state, data) : add(state, data);
+
+function addOrReplaceAll(state, data) {
+  let newIds = data
+    .filter(x => !state.byId.hasOwnProperty(x.id))
+    .map(x => x.id);
+
+  let byId = data.reduce(
+    (byId, x) => {
+      byId[x.id] = x;
+      return byId;
+    },
+    { ...state.byId }
+  );
+
+  return { ...state, allIds: [...state.allIds, ...newIds], byId };
+}
+
 const merge = (state, data, merger) =>
   replace(state, merger(state.byId[data.id], data));
+
+function mergeSafe(state, data, merger) {
+  if (!state.byId.hasOwnProperty(data.id)) {
+    throw new Error(`can't merge id ${data.id}: not found`);
+  }
+
+  return merge(state, data, merger);
+}
+
+const mergeAll = (state, data, merger) =>
+  replaceAll(state, data.map(x => merger(state.byId[x.id], x)));
+
+function mergeAllSafe(state, data, merger) {
+  let notExisting = data
+    .filter(x => !state.byId.hasOwnProperty(x.id))
+    .map(x => x.id);
+
+  if (notExisting.length) {
+    throw new Error(
+      `can't merge ids ${notExisting.join(", ")}: they're not found`
+    );
+  }
+
+  return mergeAll(state, data, merger);
+}
 
 function remove(state, id) {
   let byId = { ...state.byId };
@@ -44,12 +205,36 @@ function remove(state, id) {
   };
 }
 
+function removeAll(state, ids) {
+  ids = new Set(ids);
+  let allIds = state.allIds.filter(i => !ids.has(i));
+
+  let byId = allIds.reduce((byId, i) => {
+    byId[i] = state.byId[i];
+    return byId;
+  }, {});
+
+  return { ...state, allIds, byId };
+}
+
 function move(state, from, to) {
   let allIds = state.allIds.filter(i => i !== from).concat([to]);
   let byId = { ...state.byId };
   byId[to] = { ...byId[from] };
   delete byId[from];
   return { ...state, allIds, byId };
+}
+
+function moveSafe(state, from, to) {
+  if (!state.byId.hasOwnProperty(from)) {
+    throw new Error(`can't move id ${from} to ${to}: ${from} not found`);
+  }
+
+  if (state.byId.hasOwnProperty(to)) {
+    throw new Error(`can't move id ${from} to ${to}: ${to} already exists`);
+  }
+
+  return move(state, from, to);
 }
 
 export function reducer(
@@ -60,53 +245,70 @@ export function reducer(
   actionTypes = { ...DEFAULT_ACTION_TYPES, ...actionTypes };
   options = { ...DEFAULT_OPTIONS, ...options };
 
+  let reducers = {};
+
+  if (options.safe === true) {
+    reducers.create = createSafe;
+    reducers.createAll = createAllSafe;
+    reducers.add = addSafe;
+    reducers.addAll = addAllSafe;
+    reducers.replace = replaceSafe;
+    reducers.replaceAllSafe = replaceAllSafe;
+    reducers.move = moveSafe;
+    reducers.merge = mergeSafe;
+    reducers.mergeAll = mergeAllSafe;
+  } else if (options.safe === false) {
+    reducers.create = create;
+    reducers.createAll = createAll;
+    reducers.add = add;
+    reducers.addAll = addAll;
+    reducers.replace = replace;
+    reducers.replaceAll = replaceAll;
+    reducers.move = move;
+    reducers.merge = merge;
+    reducers.mergeAll = mergeAll;
+  } else {
+    reducers.create = options.safe.create ? createSafe : create;
+    reducers.createAll = options.safe.createAll ? createAllSafe : createAll;
+    reducers.add = options.safe.add ? addSafe : add;
+    reducers.addAll = options.safe.addAll ? addAllSafe : addAll;
+    reducers.replace = options.safe.replace ? replaceSafe : replace;
+    reducers.replaceAll = options.safe.replaceAll ? replaceAllSafe : replaceAll;
+    reducers.move = options.safe.move ? moveSafe : move;
+    reducers.merge = options.safe.merge ? mergeSafe : merge;
+    reducers.mergeAll = options.safe.mergeAll ? mergeAllSafe : mergeAll;
+  }
+
   return (state = defaultState, action) => {
     switch (action.type) {
+      case actionTypes.clear:
+        return { ...state, allIds: [], byId: {} };
       case actionTypes.create:
-        if (state.byId.hasOwnProperty(action.data.id)) {
-          throw new Error(`can't create id ${action.data.id}: already exists`);
-        }
-
-        return create(state, action.data, options.creator);
+        return reducers.create(state, action.data, options.creator);
+      case actionTypes.createAll:
+        return reducers.createAll(state, action.data, options.creator);
       case actionTypes.add:
-        if (state.byId.hasOwnProperty(action.data.id)) {
-          throw new Error(`can't add id ${action.data.id}: already exists`);
-        }
-
-        return add(state, action.data);
+        return reducers.add(state, action.data);
+      case actionTypes.addAll:
+        return reducers.addAll(state, action.data);
       case actionTypes.replace:
-        if (!state.byId.hasOwnProperty(action.data.id)) {
-          throw new Error(`can't replace id ${action.data.id}: not found`);
-        }
-
-        return replace(state, action.data);
+        return reducers.replace(state, action.data);
+      case actionTypes.replaceAll:
+        return reducers.replaceAll(state, action.data);
       case actionTypes.move:
-        if (!state.byId.hasOwnProperty(action.from)) {
-          throw new Error(
-            `can't move id ${action.from} to ${action.to}: ${action.from} not found`
-          );
-        }
-        if (state.byId.hasOwnProperty(action.to)) {
-          throw new Error(
-            `can't move id ${action.from} to ${action.to}: ${action.to} already exists`
-          );
-        }
-
-        return move(state, action.from, action.to);
+        return reducers.move(state, action.from, action.to);
       case actionTypes.addOrReplace:
-        if (state.byId.hasOwnProperty(action.data.id)) {
-          return replace(state, action.data);
-        } else {
-          return add(state, action.data);
-        }
+        return addOrReplace(state, action.data);
+      case actionTypes.addOrReplaceAll:
+        return addOrReplaceAll(state, action.data);
       case actionTypes.merge:
-        if (!state.byId.hasOwnProperty(action.data.id)) {
-          throw new Error(`can't merge id ${action.data.id}: not found`);
-        }
-
-        return merge(state, action.data, options.merger);
+        return reducers.merge(state, action.data, options.merger);
+      case actionTypes.mergeAll:
+        return reducers.mergeAll(state, action.data, options.merger);
       case actionTypes.remove:
         return remove(state, action.id);
+      case actionTypes.removeAll:
+        return removeAll(state, action.id);
       default:
         return state;
     }
