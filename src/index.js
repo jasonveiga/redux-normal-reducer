156be5b0c2dbd8b3cc427680e728b25922f79f18
Jason Veiga <jasonveiga@me.com>
2019-06-12 18:33:49 -0600

    added some utility functions

--- src/index.js
+++ src/index.js
@@ -112,3 +112,13 @@ export function reducer(
     }
   };
 }
+
+export const toArray = state => state.allIds.map(i => state.byId[i]);
+export const map = (state, fn) => toArray(state).map(fn);
+export const sort = (state, fn) => toArray(state).sort(fn);
+export const sortIds = (state, fn) =>
+  state.allIds.sort(fn).map(i => state.byId[i]);
+export const forEach = (state, fn) => toArray(state).forEach(fn);
+export const filter = (state, fn) => toArray(state).filter(fn);
+export const filterIds = (state, fn) =>
+  state.allIds.filter(fn).map(i => state.byId[i]);
